36c5fbd76a970d25d4ba9ba91916409257165fd3
Liyanapk <liyana.kp8590@gmail.com>
2025-01-14 12:37:24 +0530

    progress

--- src/app/(front-end)/page.tsx
+++ src/app/(front-end)/page.tsx
@@ -1,13 +1,14 @@
 import dynamic from "next/dynamic";
 
+const Banner = dynamic(() => import("@/components/banner/Banner"));
 const ContentSection = dynamic(() => import("@/components/content/Content"));
 const Catagory = dynamic(() => import("@/components/catagory/Catagory"));
 const InstructorPage = dynamic(() => import("@/components/instructor/Instructor"));
 const FeedBack = dynamic(() => import("@/components/feedback/FeedBack"));
-const SendEmail = dynamic(() => import("@/components/email/Email"));
-const BlogPost = dynamic(() => import("@/components/post/BlogPost"));
 const Event = dynamic(() => import("@/components/events/Event"));
-const Banner = dynamic(() => import("@/components/banner/Banner"));
+const BlogPost = dynamic(() => import("@/components/post/BlogPost"));
+const SendEmail = dynamic(() => import("@/components/email/Email"));
+
 
 export default function HomePage() {
   return (
